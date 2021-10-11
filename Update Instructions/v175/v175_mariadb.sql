a38c2ef3884cb45b52325757e78ba51f12aac933
Johan Filipsson <johan@domitor.se>
2021-10-11 14:48:17 +0200

    Naming the vue instance

--- Update Instructions/v175/v175_mariadb.sql
+++ Update Instructions/v175/v175_mariadb.sql
@@ -1,34 +1,37 @@
-ALTER TABLE `security_user` ADD COLUMN `LegalIdNumber` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `CompanyName` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `Address` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `ZipCode` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `City` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `County` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `Country` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `LastLoginMethod` VARCHAR(300) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `AllowSmsNotifications` tinyint(1) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `AllowEmailNotifications` tinyint(1) NULL DEFAULT NULL;
-ALTER TABLE `security_user` ADD COLUMN `AllowPublicProfile` tinyint(1) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `LegalIdNumber` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `CompanyName` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `Address` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `ZipCode` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `City` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `County` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `Country` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `LastLoginMethod` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `AllowSmsNotifications` tinyint(1) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `AllowEmailNotifications` tinyint(1) NULL DEFAULT NULL;
+ALTER TABLE `security_User` ADD COLUMN `AllowPublicProfile` tinyint(1) NULL DEFAULT NULL;
 
 
-CREATE TABLE IF NOT EXISTS `security_usersetting` (
+CREATE TABLE IF NOT EXISTS `security_UserSetting` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `UserId` varchar(300) DEFAULT NULL,
   `Code` varchar(300) DEFAULT NULL,
   `Value` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `USRSETTING_IDX_1` (`UserId`,`Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 DROP TABLE `sysmodel_DataViewItem`;
 
-ALTER TABLE `sysmodel_functionitem` DROP COLUMN `ActionUserInterfaceMetaCode`;
+ALTER TABLE `sysmodel_FunctionItem` DROP COLUMN `ActionUserInterfaceMetaCode`;
 
-ALTER TABLE `sysmodel_functionitem` ADD COLUMN `ActionMetaCode` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `sysmodel_FunctionItem` ADD COLUMN `ActionMetaCode` VARCHAR(300) NULL DEFAULT NULL;
 
-ALTER TABLE `sysmodel_functionitem` ADD COLUMN `ActionMetaType` VARCHAR(300) NULL DEFAULT NULL;
+ALTER TABLE `sysmodel_FunctionItem` ADD COLUMN `ActionMetaType` VARCHAR(300) NULL DEFAULT NULL;
 
-ALTER TABLE `sysmodel_userinterfacestructureitem` DROP COLUMN `DataViewMetaCode`,`DataViewColumn1MetaCode`,`DataViewColumn2MetaCode`;
+ALTER TABLE `sysmodel_UserInterfaceStructureItem` DROP COLUMN `DataViewMetaCode`;
+ALTER TABLE `sysmodel_UserInterfaceStructureItem` DROP COLUMN `DataViewColumn1MetaCode`;
+ALTER TABLE `sysmodel_UserInterfaceStructureItem` DROP COLUMN `DataViewColumn2MetaCode`;
 
-ALTER TABLE `sysmodel_viewitem` ADD COLUMN `FilePath` VARCHAR(300) NULL DEFAULT NULL;
+
+ALTER TABLE `sysmodel_ViewItem` ADD COLUMN `FilePath` VARCHAR(300) NULL DEFAULT NULL;
 
