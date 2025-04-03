45f246c9fcda65681e299fdc72e6681c2f6805ba
Johan Filipsson <johan@domitor.se>
2025-04-03 14:47:45 +0200

    N/A

--- Update Instructions/v200/v200.sql
+++
@@ -1,5 +0,0 @@
-alter table sysdata_EventLog add ProductID nvarchar(300)
-GO
-alter table sysdata_EventLog add ProductTitle nvarchar(300)
-GO
-UPDATE sysdata_EventLog SET ProductID='',ProductTitle='' WHERE ProductID IS NULL
